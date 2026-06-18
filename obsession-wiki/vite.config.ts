2b478dea20dbd5fd2ba4a25117e11a7c0b4150bd
d5nf9kcskk-byte <d5nf9kcskk@privaterelay.appleid.com>
2026-06-18 17:19:42 -0400

    Remove obsession-wiki from NWSA repo

--- obsession-wiki/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/ggmuze/',
-  server: { port: 5174 }
-})
